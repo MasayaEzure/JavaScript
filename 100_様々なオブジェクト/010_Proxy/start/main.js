b31fa888c293b5e5a97c21da2d8d7ef5078f078d
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-03-14 23:43:14 +0900

    Proxyの学習まで完了

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,25 @@
 const targetObj = { a: 0 };
+
+const handler = {
+  set: function (target, prop, value) {
+    console.log(`[set]: ${prop}`);
+    target[prop] = value;
+    // throw new Error("cannnot add prop");
+  },
+  get: function (target, prop) {
+    if (target.hasOwnProperty(prop)) {
+      return target[prop];
+    } else {
+      return "-1";
+    }
+  },
+  delete: function (target, prop) {
+    console.log(`[delete]: ${prop}`);
+    delete target[prop];
+  },
+};
+
+const pxy = new Proxy(targetObj, handler);
+pxy.a = 1;
+console.log(pxy.b);
+delete pxy.a;
