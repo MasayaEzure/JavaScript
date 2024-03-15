e09d443dbdbbd34da653f80fa0173f46e159cffd
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-03-15 17:58:53 +0900

    JSONの学習を完了

--- 100_様々なオブジェクト/060_JSON/start/main.js
+++ 100_様々なオブジェクト/060_JSON/start/main.js
@@ -1,1 +1,15 @@
-const obj = {a: 0, b: 1, c: 2};
+const obj = { a: 0, b: 1, c: 2 };
+
+function replacer(value) {
+  if (value < 1) return;
+  return value;
+}
+
+// const json = JSON.stringify(obj, replacer);
+const json = JSON.stringify(obj, ["a", "c"]);
+console.log(json);
+console.log(typeof json);
+
+const obj2 = JSON.parse(json);
+console.log(obj2);
+console.log(typeof obj2);
