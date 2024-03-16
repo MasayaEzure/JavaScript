e0929ccd7f26b8ac457c7a10cb323bcb0f24f3a3
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-03-16 23:56:23 +0900

    Refletの学習まで完了

--- 100_様々なオブジェクト/020_Reflect/start/main.js
+++ 100_様々なオブジェクト/020_Reflect/start/main.js
@@ -5,20 +5,31 @@ class C {
   }
 }
 
+const obj = new C(1, 2);
+console.log(obj);
+const obj2 = Reflect.construct(C, [1,2]);
+console.log(obj2);
 
-// const bob = {
-//   name: 'Bob',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   }
-// }
+console.log("a" in obj2);
+console.log(Reflect.has(obj, "a"));
 
-// const tom = {
-//   name: 'Tom',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   },
-//   get hello() {
-//     return this._hello();
-//   },
-// }
+const bob = {
+  name: 'Bob',
+  _hello: function () {
+    console.log(`hello ${this.name}`);
+  }
+}
+
+const tom = {
+  name: 'Tom',
+  _hello: function () {
+    console.log(`hello ${this.name}`);
+  },
+  get hello() {
+    console.log(this);
+    return this._hello();
+  },
+}
+
+tom.hello;
+Reflect.get(tom, "hello", bob); // 第三引数は bind と同様の挙動をとる
